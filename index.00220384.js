e1ff46c8219cd8ede08cd0ed019f80a4af6d77fa
renata <renatalinec@gmail.com>
2024-12-11 16:20:07 -0300

    make build

---
+++ index.00220384.js
@@ -0,0 +1,2 @@
+const t=document.querySelectorAll(".shop__tab_list_link-item");for(let e=0;e<t.length;e++)t[e].addEventListener("click",function(){for(let l=0;l<t.length;l++)t[e].classList.remove("active");this.classList.add("active")});
+//# sourceMappingURL=index.00220384.js.map
